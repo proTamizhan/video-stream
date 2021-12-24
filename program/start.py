0b44c760207ca517eaaf6793150d3ec137280a6a
Kittuthemeow <89501967+VipBoys777@users.noreply.github.com>
2021-12-24 08:32:07 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -60,7 +60,7 @@ async def start_(client: Client, message: Message):
 ┏━━━━━━━━━━━━━━━━
 ┣ ₪ *video&musicplayer* 
 ┣ ₪ *Iam Very Fast vc player 
-┗━━━━━━━━━━━━━━━━ """,
+┗━━━━━━━━━━━━━━━━ """
 
         reply_markup=InlineKeyboardMarkup(
             [
